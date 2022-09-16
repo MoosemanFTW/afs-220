2fe51fc6be67f49b487d36be145e0a288f7dffbd
MoosemanFTW <justin.milligan@bryanuniversity.edu>
2022-09-16 13:04:34 -0700

    save point

--- week4/docker_apache/tourSite/urls.py
+++ week4/docker_apache/tourSite/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('bellagio/', views.bellagio, name='bellagio'),
+    path('stratosphere/', views.stratosphere, name='stratosphere'),
+    path('freemont/', views.freemont, name='freemont')
     # new page path
     # path('pagename/', views.filename, name='pagename')
 ]
--- week4/docker_apache/tourSite/views.py
+++ week4/docker_apache/tourSite/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'pages/home.html')
+
+def bellagio(request):
+    return render(request, 'pages/bellagio.html')
+
+def stratosphere(request):
+    return render(request, 'pages/stratosphere.html')
+
+def freemont(request):
+    return render(request, 'pages/freemont.html')
